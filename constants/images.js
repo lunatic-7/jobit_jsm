1ad015a8c4417a87ece984bd64061196b2ac5941
Wasif Nadeem <wasif1607@gmail.com>
2023-09-19 17:36:04 +0530

    first commit

---
+++ constants/images.js
@@ -0,0 +1,5 @@
+import profile from "../assets/images/wasif.jpg";
+
+export default {
+  profile,
+};
---
+++ utils/index.js
@@ -0,0 +1,8 @@
+// This function checks if the url is an image url or not
+export const checkImageURL = (url) => {
+    if (!url) return false
+    else {
+        const pattern = new RegExp('^https?:\\/\\/.+\\.(png|jpg|jpeg|bmp|gif|webp)$', 'i');
+        return pattern.test(url);
+    }
+};
